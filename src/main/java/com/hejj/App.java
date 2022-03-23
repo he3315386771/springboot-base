c134a5d561e3dca44019dd27ceaff1e27e7aeabc
he3315386771 <3315386771@qq.com>
2022-03-23 14:29:08 +0800

    改名

--- src/main/java/com/hejj/App.java
+++ src/main/java/com/hejj/App.java
@@ -1,4 +1,4 @@
-package com.kyy;
+package com.hejj;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
--- src/main/java/com/hejj/control/HelloController.java
+++ src/main/java/com/hejj/control/HelloController.java
@@ -1,4 +1,4 @@
-package com.kyy.control;
+package com.hejj.control;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
