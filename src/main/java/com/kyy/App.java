5a25e8e1b8947bae43c4b93ddabd1172477e8e0c
he3315386771 <3315386771@qq.com>
2022-03-23 14:19:46 +0800

    打war包

--- src/main/java/com/kyy/App.java
+++ src/main/java/com/kyy/App.java
@@ -5,18 +5,25 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 /**
  * Hello world!
  *
  */
 @SpringBootApplication
-public class App {
+public class App extends SpringBootServletInitializer {
     private static Logger logger = LoggerFactory.getLogger(App.class);
 
     public static void main( String[] args ) {
         SpringApplication app = new SpringApplication(App.class);
         app.setBannerMode(Banner.Mode.OFF);//banner关闭
         app.run(args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(App.class);
     }
 }
